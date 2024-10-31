d54b780d2c0a1dd2abbd50d75013ebe04a0bec74
Kurtis Rios <kurtis@altruist.com>
2024-10-30 18:21:29 -0700

    Initialize TS files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ['./src/**/*.{js,jsx}', './src/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
