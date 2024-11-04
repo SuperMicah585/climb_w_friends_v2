2f5ddd1ec61c791600c530f746d05a04b22899c3
SuperMicah585 <micahphlps@gmail.com>
2024-11-03 22:39:38 -0800

    Added marker placement on search and conditional rendering for shapes

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,12 +1,13 @@
 // Import Mapbox GL, and CSS
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import Search from './mapComponents/search'
 import ActivityFeed from './mapComponents/activityFeed';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import {ClimbsTableResponse} from '../types/interfaces'
+import {createMarker,createClimbingShapes,updateLayerVisibility} from './mapComponents/mapLayers'
 
 
-console.log("hi")
 // Set Mapbox access token
 mapboxgl.accessToken = import.meta.env.VITE_MAP_BOX_KEY;
 
@@ -17,6 +18,16 @@ type MapProps = {
 const Map: React.FC<MapProps> = ({zoomLevel}) => {
   const map = useRef<mapboxgl.Map>();
   const mapContainer = useRef<HTMLDivElement>(null);
+  const [selectedClimb,setSelectedClimb] = useState<ClimbsTableResponse | null>(null)
+  const [currentMarker,setCurrentMarker] = useState<any>(null)
+  const [polygonOrCircleDisplay, setpolygonOrCircleDisplay] = useState<boolean>(false)
+  const [mapLoaded, setMapLoaded] = useState(false);
+ 
+
+  const selectedClimbCallBack = (climbData:ClimbsTableResponse) =>{
+
+    setSelectedClimb(climbData)
+  }
 
   useEffect(() => {
     if (mapContainer.current) {
@@ -28,15 +39,65 @@ const Map: React.FC<MapProps> = ({zoomLevel}) => {
         projection: 'globe', // Enable the globe projection
       });
     }
+
+    map.current?.on("zoomend", () => {
+      const currentZoom = map.current?.getZoom() ?? 2;
+      if(currentZoom>12){
+      setpolygonOrCircleDisplay(true);
+      }
+      else{
+        setpolygonOrCircleDisplay(false);
+      }
+    });
+
+
+    createClimbingShapes(map)
     map.current?.addControl(new mapboxgl.NavigationControl(), 'bottom-left');
     return () => map.current?.remove(); // Clean up on unmount
   }, []);
 
 
+  useEffect(() => {
+    if (map.current) {
+      map.current.on("load", () => {
+        setMapLoaded(true); // Map has finished loading
+      });
+    }
+  }, [map]);
+
+
+  useEffect(() => {
+    if (selectedClimb && map?.current) {
+      if (currentMarker) {
+        currentMarker.remove();
+      }
+  
+
+      const newMarker = createMarker(
+        selectedClimb["Area Latitude"],
+        selectedClimb["Area Longitude"],
+        selectedClimb.Route,
+        map.current
+      );
+
+      
+  
+      setCurrentMarker(newMarker);
+    }
+  }, [selectedClimb]);
+
+useEffect(() => {
+  if(mapLoaded){
+  updateLayerVisibility(map,polygonOrCircleDisplay)
+  }
+},[polygonOrCircleDisplay,mapLoaded])
+  
+
+  
 
   return (
     <> 
-  <div className = 'absolute top-0 left-0'> <Search/> </div>
+  <div className = 'absolute top-0 left-0'> <Search selectedClimbCallBack = {selectedClimbCallBack} /> </div>
   <ActivityFeed/>
 
   <div className='w-screen h-screen' ref={mapContainer}/>
