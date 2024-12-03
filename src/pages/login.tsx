ebe158269d5d8de67f612e4c56e66a7ee5489bcc
SuperMicah585 <micahphlps@gmail.com>
2024-12-03 13:32:44 -0800

    updated frontend files + gitingore for app settings

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,20 +5,39 @@ import HomeNavBar from './homeComponents/homeNavBar';
 import camoBackGroundImage from './homeComponents/black_camo.jpeg';
 const AuthComponent: React.FC = () => {
   return (
-    <>
-      <div className="absolute left-0 top-0 flex h-screen w-screen items-center justify-center bg-zinc-800">
-        <img
-          className="h-full w-full opacity-10"
+
+      <div className="absolute left-0 top-0 flex h-screen w-screen flex-col bg-zinc-800 overflow-y-scroll">
+        
+          <HomeNavBar />
+
+      <img
+          className="h-full w-full object-cover opacity-10 absolute pointer-events-none"
           src={camoBackGroundImage}
           alt="Description of the image"
         />
 
-        <div className="pointer-events-auto absolute left-0 top-0">
-          <HomeNavBar />
-        </div>
+
       </div>
-    </>
+
   );
 };
 
+
 export default AuthComponent;
+//climb
+//5432
+
+/*
+
+Installation Directory: /Library/PostgreSQL/16
+Server Installation Directory: /Library/PostgreSQL/16
+Data Directory: /Library/PostgreSQL/16/data
+Database Port: 5432
+Database Superuser: postgres
+Operating System Account: postgres
+Database Service: postgresql-16
+Command Line Tools Installation Directory: /Library/PostgreSQL/16
+pgAdmin4 Installation Directory: /Library/PostgreSQL/16/pgAdmin 4
+Stack Builder Installation Directory: /Library/PostgreSQL/16
+Installation Log: /tmp/install-postgresql.log
+*/
