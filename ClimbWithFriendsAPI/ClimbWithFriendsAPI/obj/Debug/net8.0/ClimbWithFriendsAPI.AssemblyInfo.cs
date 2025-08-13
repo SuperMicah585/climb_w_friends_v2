28c2d99baf725cdbc818d0f42621fc45cfe468c4
Micah Phelps <micahphlps@gmail.com>
2025-08-13 13:19:26 -0700

    stuff

--- ClimbWithFriendsAPI/ClimbWithFriendsAPI/obj/Debug/net8.0/ClimbWithFriendsAPI.AssemblyInfo.cs
+++ ClimbWithFriendsAPI/ClimbWithFriendsAPI/obj/Debug/net8.0/ClimbWithFriendsAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClimbWithFriendsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9812e190d0ddbeab3d412a3ebb8087e3d2b694ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64cbeb49f80b954acf08b3ca5817546b51ac0376")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClimbWithFriendsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClimbWithFriendsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
